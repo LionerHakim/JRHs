82b94c8a17e2e8152b340738a90e26934a43446d
J. <jefri.tegal12@gmail.com>
2026-03-12 10:48:29 +0700

    Remove duplicate song entry from playlist
    Removed duplicate entry for 'Digeleng Broo' from the playlist.

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -5,11 +5,10 @@ import { Play, Pause, SkipForward, SkipBack, Volume2, VolumeX, ChevronDown, Musi
 // Using reliable public domain / creative commons audio links for demonstration
 const playlist = [
   { title: 'Budi Doremi', artist: '', src: '/data/musik/Budi-Doremi.mp3' },
-  { title: 'Digeleng Broo', artist: '', src: '/data/musik/Digeleng-Broo.mp3' },
   { title: 'Rahasia Hati', artist: '', src: '/data/musik/Rahasia-Hati.mp3' },
   { title: 'Tak Ada Ujungnya', artist: '', src: '/data/musik/Tak-AdaUjungnya.mp3' },
   { title: 'Tourner Dans Le Vide', artist: '', src: '/data/musik/Tourner-DansLeVide.mp3' },
-  { title: 'Who Knows', artist: '', src: '/data/musik/Who-Knows.mp3' },
+  { title: 'Who Knows', artist: '', src: '/data/musik/Who-Knows.mp3' }
 ];
 
 export default function MusicPlayer() {
