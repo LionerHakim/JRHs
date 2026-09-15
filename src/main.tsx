f4d585614cbc4b0f4fd673e5577afe4d2546c8d3
J. <188508746+LionerHakim@users.noreply.github.com>
2026-09-15 23:34:38 +0700

    load scoped portfolio fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './jrh-system.css';
+import './jrh-fixes.css';
 
 const root = document.getElementById('root');
 
